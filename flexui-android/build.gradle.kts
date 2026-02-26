d109ef5a5c7f5709283643a9217eaf5fe6b0107e
Jarvis (AI) <venkatesh3007@users.noreply.github.com>
2026-02-26 16:29:46 +0000

    fix: Bump AGP 8.7.3, Kotlin 2.1.0, Gradle 8.11.1, compileSdk 35, JVM 17
    Fixes HasConvention error from Gradle 9.x incompatibility with old AGP/Kotlin

--- flexui-android/build.gradle.kts
+++ flexui-android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.library") version "8.2.1" apply false
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.library") version "8.7.3" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 // Repositories configured in settings.gradle.kts
